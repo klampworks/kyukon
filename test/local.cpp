efdc856b9e7725ab3f3754ec5f9e6ebc20087b7f
kyukon <wolfvbear@gmail.com>
2013-12-19 11:17:20 +0000

    Added fucntion for parsing index files.

--- test/local.cpp
+++ test/local.cpp
@@ -5,5 +5,24 @@
 
 BOOST_AUTO_TEST_CASE( free_test_function ) {
 
+	prase_index();
 	BOOST_CHECK( true /* test assertion */ );
+}
+
+std::vector<std::string> parse_index() {
+
+	const char *filename = "/var/www/localhost/htdocs/index/html";
+
+	std::vector<std::string> ret;
+
+	std::ifstream ifs(filename);
+	std::string tmp;
+
+	while(std::getline(ifs, tmp)) {
+		
+		ret.push_back(tmp);
+	}
+
+	//Copy ellision.
+	return std::move(ret);
 }
