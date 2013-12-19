91eff6fc827dc82f861151a3fa97360b86f10981
kyukon <wolfvbear@gmail.com>
2013-12-19 14:03:20 +0000

    Removed the fucking mutex from domain_settings because it was causing double free errors. Maybe use a shared_ptr or somethingg in future to get around the deleted copy constructor.

--- kyukon.hpp
+++ kyukon.hpp
@@ -11,7 +11,6 @@
 struct domain_settings {
 
 	std::priority_queue<task*, std::vector<task*>, task> task_list;
-	std::mutex *list_mutex;
 	long interval;
 	std::function<void()> fillup;
 	bool do_fillup;
@@ -19,22 +18,15 @@ struct domain_settings {
 	domain_settings() {
 
 		interval = 0;
-		list_mutex = new std::mutex();
 		fillup = nullptr;
 		do_fillup = true;
-	}
-
-	~domain_settings() {
-
-		if (list_mutex)
-			delete list_mutex;
 	}
 };
 
 namespace kyukon {
 
 	void init(const std::vector<std::pair<std::string, bool>>&);
-	void signup(unsigned domain_id, domain_settings&&);
+	void signup(unsigned domain_id, domain_settings);
 	void add_task(task *t, unsigned);
 	void set_do_fillup(bool, unsigned);
 	//extern bool do_fillup;
