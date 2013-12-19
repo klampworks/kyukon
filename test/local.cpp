3051ed5c91cffa4064ec1381c95a65dc4d004dab
kyukon <wolfvbear@gmail.com>
2013-12-19 11:24:34 +0000

    Fixed typo in filepath and added range based for loop to print out the values read.

--- test/local.cpp
+++ test/local.cpp
@@ -2,16 +2,26 @@
 #include <boost/test/included/unit_test.hpp>
 #include "../kyukon.hpp"
 #include "../task.hpp"
+#include <iostream>
+#include <fstream>
+
+std::vector<std::string> parse_index(void);
 
 BOOST_AUTO_TEST_CASE( free_test_function ) {
 
-	prase_index();
-	BOOST_CHECK( true /* test assertion */ );
+	std::vector<std::string> l = parse_index();
+
+	for (const auto &s : l) {
+
+		std::cout << s << std::endl;
+	}
+
+	BOOST_CHECK( !l.empty() /* test assertion */ );
 }
 
 std::vector<std::string> parse_index() {
 
-	const char *filename = "/var/www/localhost/htdocs/index/html";
+	const char *filename = "/var/www/localhost/htdocs/index.html";
 
 	std::vector<std::string> ret;
 
